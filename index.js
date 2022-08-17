b87be9068a7dd60fbeaad0721990f92ca1d2375c
Rafael Gil <rafaelgil83@gmail.com>
2022-08-17 16:30:25 -0400

    working version

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 // Octokit.js
 // https://github.com/octokit/core.js#readme
+// git push origin v3.8.8
+// git tag -a v3.8.8 -m "my version 3.8.8"
+
+const {Octokit} = require("octokit");
 const octokit = new Octokit({
-    auth: 'personal-access-token123'
+    auth: ''
 })
 
-await octokit.request('POST /repos/{owner}/{repo}/releases', {
-    owner: 'OWNER',
-    repo: 'REPO',
-    tag_name: 'v1.0.0',
-    target_commitish: 'master',
-    name: 'v1.0.0',
-    body: 'Description of the release',
-    draft: false,
-    prerelease: false,
-    generate_release_notes: false
-})
+async function createRelease() {
+    await octokit.request('POST /repos/{owner}/{repo}/releases', {
+        owner: '',
+        repo: '',
+        tag_name: 'v3.8.8',
+        target_commitish: 'release-3.8.8-deleteme',
+        name: 'v3.8.8',
+        body: 'Description of the release',
+        draft: false,
+        prerelease: false,
+        generate_release_notes: true
+    })
+}
+
+console.log(createRelease())
